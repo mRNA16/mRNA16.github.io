f6e7be255eb9dc69cdbc82cc0a41c5448b685817
mRNA <1518926653@qq.com>
2024-11-14 01:27:14 +0800

    Site updated: 2024-11-14 01:27:11

---
+++ js/time-insert.js
@@ -0,0 +1,13 @@
+// 创建新的 div 元素
+var newDiv = document.createElement("div");
+newDiv.innerHTML = `
+      <span id="time"></span>
+      <script src="/js/time.js"></script>
+      `;
+
+// 获取目标插入位置，div1 和 div2 之间
+var div1 = document.getElementById("statistics");
+var div2 = document.getElementById("beian");
+
+// 将新 div 插入到 div1 和 div2 之间
+div1.parentNode.insertBefore(newDiv, div2);
