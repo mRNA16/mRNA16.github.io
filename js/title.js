0bae8ac730fb6feca4bb717583a54cdb40f76955
mRNA <1518926653@qq.com>
2024-11-13 18:11:32 +0800

    Site updated: 2024-11-13 18:11:30

--- js/title.js
+++ js/title.js
@@ -2,12 +2,11 @@
  var titleTime;
  document.addEventListener('visibilitychange', function () {
      if (document.hidden) {
-         document.title = '这里是mRNA的Blog';
+         document.title = '(ง •̀_•́)ง‼这里是mRNA的Blog~';
          clearTimeout(titleTime);
      }
      else {
-         $('[rel="icon"]').attr('href', "/img/back.JPG");
-         document.title = '欢迎回来！' + OriginTitle;
+         document.title = '( つ•̀ω•́)つ欢迎回来！';
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
          }, 2000);
