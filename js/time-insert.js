8c7580a1a8ddf85cd56ca2ffea7ddeb472f2dda3
mRNA <1518926653@qq.com>
2024-11-14 01:33:13 +0800

    Site updated: 2024-11-14 01:33:10

--- js/time-insert.js
+++ js/time-insert.js
@@ -6,8 +6,8 @@ newDiv.innerHTML = `
       `;
 
 // 获取目标插入位置，div1 和 div2 之间
-var div1 = document.getElementById("statistics");
-var div2 = document.getElementById("beian");
+var div1 = document.getElementsByClassName("statistics");
+var div2 = document.getElementsByClassName("beian");
 
 // 将新 div 插入到 div1 和 div2 之间
 div1.parentNode.insertBefore(newDiv, div2);
